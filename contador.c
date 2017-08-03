ded3e1be7338184c7343f3b0460ede8ef98262da
Andregarciav <andregarcia@dcc.ufmg.br>
2017-08-03 18:44:49 -0300

    Update contador.c

--- contador.c
+++ contador.c
@@ -1,12 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 int main()
 {
   int i, aux;
+  char msg[20];
+  clock_t inicio, final;
+  inicio = clock();
   for (i=0; i < 10000; i++)
   {
-    printf("%d\n", i );
+    final = clock();
+    aux = (final - inicio)/1000;
+    printf("%d\n%d\n", i , aux );
   }
   return 0;
 }
